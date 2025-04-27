eb923e66e1519539d717fc6dab50e9a930b59eb6
Krish Gupta <krishg053@gmail.com>
2025-04-27 19:04:16 +0530

    Initial Commit

---
+++ prisma/migrations/20250427133054_init/migration.sql
@@ -0,0 +1,44 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "imageUrl" TEXT,
+    "industry" TEXT,
+    "bio" TEXT,
+    "experience" INTEGER,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "IndustryInsight" (
+    "id" TEXT NOT NULL,
+    "industry" TEXT NOT NULL,
+    "salaryRanges" JSONB[],
+    "growthRate" DOUBLE PRECISION NOT NULL,
+    "demandLevel" TEXT NOT NULL,
+    "topSkills" TEXT[],
+    "marketOutlook" TEXT NOT NULL,
+    "keyTrends" TEXT[],
+    "recommendedSkills" TEXT[],
+    "lastUpdated" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "nextUpdate" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "IndustryInsight_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "IndustryInsight_industry_key" ON "IndustryInsight"("industry");
+
+-- CreateIndex
+CREATE INDEX "IndustryInsight_industry_idx" ON "IndustryInsight"("industry");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_industry_fkey" FOREIGN KEY ("industry") REFERENCES "IndustryInsight"("industry") ON DELETE SET NULL ON UPDATE CASCADE;
